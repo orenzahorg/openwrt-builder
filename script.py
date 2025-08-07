7d0d85740b5ee6a019153e91952d003d33ea56d4
Oren Zaharia <orenzah@gmail.com>
2025-08-07 10:16:12 +0300

    wip5

---
+++ script.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+import os
+import sys
+import logging
+import subprocess
+
+
+def run_command(command: list[str], cwd: str = None):
+    result = subprocess.run(command, shell=True, capture_output=True, text=True, cwd=cwd)
+    return result.stdout, result.stderr
+
+def setup_logging():
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+def main():
+    setup_logging()
+    logging.info("Script started")
+
+    # check if openwrt directory exists
+    logging.info("Checking if openwrt directory exists")
+    if not os.path.exists("~/openwrt-bkp"):
+        logging.error("openwrt directory does not exist")
+        run_command(["git", "clone", "https://github.com/openwrt/openwrt.git"])
+    else:
+        logging.info("openwrt directory exists")
+        run_command(["cp", "-r", "~/openwrt-bkp", "openwrt"])
+    # check if mtk-openwrt-feeds directory exists
+    # git clone https://git01.mediatek.com/openwrt/feeds/mtk-openwrt-feeds mtk-openwrt-feeds
+    logging.info("Checking if mtk-openwrt-feeds directory exists")
+    if not os.path.exists("~/mtk-openwrt-feeds-bkp"):
+        logging.error("mtk-openwrt-feeds directory does not exist")
+        run_command(["git", "clone", "https://git01.mediatek.com/openwrt/feeds/mtk-openwrt-feeds", "mtk-openwrt-feeds"])
+    else:
+        logging.info("mtk-openwrt-feeds directory exists")
+        run_command(["cp", "-r", "~/mtk-openwrt-feeds-bkp", "mtk-openwrt-feeds"])
+
+    
+    
+    
+    
+
+if __name__ == "__main__":
+    main()
