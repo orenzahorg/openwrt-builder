fbb75d88a9b87cc6c86255a4aad9e72715fc8401
Oren Zaharia <orenzah@gmail.com>
2025-08-11 15:07:03 +0300

    chore: update OpenWrt configuration in patch script by enabling and disabling specific options for improved build settings

--- patch.sh
+++ patch.sh
@@ -4,12 +4,12 @@ set -euo pipefail
 cd openwrt
 echo 'CONFIG_PINCTRL_MT7987=y' >> target/linux/mediatek/filogic/config-6.6
 echo 'CONFIG_COMMON_CLK_MT7987=y' >> target/linux/mediatek/filogic/config-6.6
-# echo 'CONFIG_VIRTIO_BLK=n' >> target/linux/mediatek/filogic/config-6.6
-# echo 'CONFIG_VIRTIO_NET=n' >> target/linux/mediatek/filogic/config-6.6
-# echo 'CONFIG_REMOTEPROC_CDEV=n' >> target/linux/mediatek/filogic/config-6.6
+echo 'CONFIG_VIRTIO_BLK=n' >> target/linux/mediatek/filogic/config-6.6
+echo 'CONFIG_VIRTIO_NET=n' >> target/linux/mediatek/filogic/config-6.6
+echo 'CONFIG_REMOTEPROC_CDEV=n' >> target/linux/mediatek/filogic/config-6.6
 echo 'CONFIG_MTK_SCP=n' >> target/linux/mediatek/filogic/config-6.6
-# echo 'CONFIG_MEDIATEK_NETSYS_V2=y' >> target/linux/mediatek/filogic/config-6.6
-# echo 'CONFIG_MEDIATEK_NETSYS_V3=y' >> target/linux/mediatek/filogic/config-6.6
-# sed -i '/CONFIG_EXTRA_FIRMWARE/d' target/linux/mediatek/filogic/config-6.6
-# sed -i '/CONFIG_EXTRA_FIRMWARE_DIR/d' target/linux/mediatek/filogic/config-6.6
+echo 'CONFIG_MEDIATEK_NETSYS_V2=y' >> target/linux/mediatek/filogic/config-6.6
+echo 'CONFIG_MEDIATEK_NETSYS_V3=y' >> target/linux/mediatek/filogic/config-6.6
+sed -i '/CONFIG_EXTRA_FIRMWARE/d' target/linux/mediatek/filogic/config-6.6
+sed -i '/CONFIG_EXTRA_FIRMWARE_DIR/d' target/linux/mediatek/filogic/config-6.6
 cd -
